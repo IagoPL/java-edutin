641cace26fc2f52d89e895697ef35e94602b74b3
iagoVL <iago.v.lamas@gmail.com>
2021-07-19 14:24:22 +0200

    variables

--- src/comentarios.java
+++ src/comentarios.java
@@ -1,4 +1,4 @@
-package comentarios;
+
 
 public class comentarios {
     
---
+++ src/variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args) {
+        int junto=21;// declarando e inicializando variable
+
+        System.out.println(junto);
+
+
+        int separado;//declarando variable
+
+        separado=15;//inicializando
+
+        System.out.println(separado);
+    }
+}
