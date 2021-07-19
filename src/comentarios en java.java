c9e91fed2261df5261962c2d6d2acc1ea53dde7d
iagoVL <iago.v.lamas@gmail.com>
2021-07-19 13:00:20 +0200

    comentarios

---
+++ src/comentarios en java.java
@@ -0,0 +1,20 @@
+package comentarios en java;
+
+public class comentarios en java {
+    
+        public static Void main(String[] args) {
+
+          // Esto es un coentario en Java de una linea
+          
+          
+          // Ejemplo: esto es una impreion
+          System.out.println("hola");
+
+          /*
+          este es un comentarios
+          que permite hacer
+          multilinea
+          */
+          System.out.println("hola2");
+        }
+}
