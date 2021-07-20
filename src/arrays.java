0aaaf3a31850d7876b9bf914de57ce955bc76de4
iagoVL <iago.v.lamas@gmail.com>
2021-07-20 16:52:40 +0200

    arrays

---
+++ src/arrays.java
@@ -0,0 +1,18 @@
+public class arrays {
+    public static void main(String[] args) {
+        int [] numero = new int [10];
+
+        System.out.println("numero sin definir: "+numero[8]);
+
+
+        numero [0] = 10;
+        numero [2] = 20;
+        numero [3] = 30;
+        numero [4] = 40;
+        System.out.println("numero definido: "+numero[2]);
+
+        int [] numeros ={10,22,34,45,54,67,72,84,99,10};
+
+        System.out.println(numeros[ 2 ]);
+    }
+}
