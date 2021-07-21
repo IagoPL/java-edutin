9076391304d6f3943ca5d8e7931977a1d5d5bc75
iagoVL <iago.v.lamas@gmail.com>
2021-07-21 18:47:49 +0200

    ejercicio 3 acavado

--- src/sentencias_situacionales.java
+++ src/sentencias_situacionales.java
@@ -1,11 +1,54 @@
 import java.util.Scanner;
 import java.util.Arrays;
-
+import java.util.*;
+import java.lang.*;
 public class sentencias_situacionales {
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        System.out.println("ingrese 5 numeros");
+        
+/*
 
+        String cadena="hola como estas";
+        String [] array=cadena.split(" ");
+        String invertido="";
+        for(int i=array.length-1;i>=0; i--){
+
+            invertido=invertido+" "+array[i];
+        }
+        System.out.println(invertido);
+*/
+
+
+
+Scanner scan = new Scanner (System.in);
+        System.out.println("ingrese 5 numeros separados por comas");
+
+        String numerosStr = scan.nextLine();
+
+        String cadena = numerosStr;
+
+
+        String [] array=cadena.split(",");
+        String invertido="";
+        for(int i=array.length-1;i>=0; i--){
+
+            invertido=invertido+" "+array[i];
+        }
+        System.out.println(invertido);
+/*
+        Scanner scan = new Scanner (System.in);
+        System.out.println("ingrese 5 numeros separados por comas");
+
+        String numerosStr = scan.nextLine();
+
+        String cadena = numerosStr;
+        String str = cadena;
+        String[] arrOfStr = str.split(",");
+
+        for (String a : arrOfStr)
+            System.out.println(a);
+*/
+}
+/*
         String numerosStr = scan.nextLine();
 
         String cadena = numerosStr;
@@ -15,4 +58,5 @@ public class sentencias_situacionales {
         System.out.println("sus siglas son: "+numerosStr.substring(4,5)+", "+numerosStr.substring(3,4)+", "+numerosStr.substring(2,3)+", "+numerosStr.substring(1,2)+", "+numerosStr.substring(0,1));
         
     }
+}*/
 }
