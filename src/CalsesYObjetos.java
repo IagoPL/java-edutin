25ff508a6c23915e8b36780652dc79448a288b74
iagoVL <iago.v.lamas@gmail.com>
2021-07-26 11:26:13 +0200

    clases y objetos

---
+++ src/CalsesYObjetos.java
@@ -0,0 +1,15 @@
+public class CalsesYObjetos {
+    String marca;
+    String color;
+    
+
+    public static void main(String[] args) {
+        CalsesYObjetos objeto1 = new CalsesYObjetos();
+
+        objeto1.color ="rojo";
+        objeto1.marca = "marca";
+
+        System.out.println("el color del objeto uno es: "+objeto1.color);
+    }
+    
+}
